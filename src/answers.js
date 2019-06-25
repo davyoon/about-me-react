1c51a0bbfd4ed709ad1a7d57287d0c9624ce7341
David Yoon <david_yoon86@hotmail.com>
2019-06-25 16:30:58 -0400

    separate answers component

---
+++ src/answers.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Row, Container, Table} from 'react-bootstrap';
+
+
+
+const Answers = (props) => {
+
+  return(
+        <Container className="score-container">
+          <Row>
+            <h2>Congratulations! you got {props.correct} out of 20 correct</h2>
+          </Row>
+          <Row className="answer-container">
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <th>#</th>
+                <th>Question</th>
+                <th>Answer</th>
+              </thead>
+              <tbody>
+                {props.questions.map((question, idx) => {
+                  return (
+                    <tr>
+                      <td>{idx + 1}</td>
+                      <td>{question.question}</td>
+                      <td>{question.choices.reduce((acc, choice) => {
+                           return acc = choice.answer ? choice.text : acc;
+                          }, "")}
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </Table>
+          </Row>
+        </Container>  
+  )
+}
+
+export default Answers;
